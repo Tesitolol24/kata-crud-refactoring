27caa9983bb6b0656dd2e1cb03613a432a7d61cc
Juan Pablo Mesa <tesitolol24@gmail.com>
2021-03-01 10:41:25 -0500

    Se realizo un diseño grafico y se separarons los elementos en las diferentes carpetas

---
+++ front/src/components/Form.js
@@ -0,0 +1,84 @@
+import React, { useRef, useContext, useState } from "react";
+const HOST_API = "http://localhost:8080/api";
+
+const Form = (parameterStore) => {
+    const Store = parameterStore.parameterStore;
+    const formRef = useRef(null);
+    const { dispatch, state: { todo } } = useContext(Store);
+    const item = todo.item;
+    const [state, setState] = useState(item);
+
+    const onAdd = (event) => {
+        event.preventDefault();
+
+        const request = {
+            name: state.name,
+            id: null,
+            completed: false
+        };
+
+        if (request["name"] !== ""&&request["name"] !== undefined) {
+            fetch(HOST_API + "/todo", {
+                method: "POST",
+                body: JSON.stringify(request),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((todo) => {
+                    dispatch({ type: "add-item", item: todo });
+                    setState({ name: "" });
+                    formRef.current.reset();
+                });
+        }else{
+            alert("No puedes agregar una tarea vacia");
+        }
+        
+    }
+
+    const onEdit = (event) => {
+        event.preventDefault();
+
+        const request = {
+            name: state.name,
+            id: item.id,
+            isCompleted: item.isCompleted
+        };
+
+        console.log(request);
+        if (request["name"] !== ""&&request["name"] !== undefined) {
+            fetch(HOST_API + "/todo", {
+                method: "PUT",
+                body: JSON.stringify(request),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((todo) => {
+                    dispatch({ type: "update-item", item: todo });
+                    setState({ name: "" });
+                    formRef.current.reset();
+                });
+        }else{
+            alert("No puedes agregar una tarea vacia");
+        }
+    }
+
+    return <form ref={formRef}>
+        <input
+            type="text"
+            name="name"
+            placeholder="¿Qué piensas hacer hoy?"
+            defaultValue={item.name}
+            className="inputToDo"
+            onChange={(event) => {
+                setState({ ...state, name: event.target.value })
+            }}  ></input>
+        {item.id && <button className="button" onClick={onEdit}>Actualizar</button>}
+        {!item.id && <button className="button" onClick={onAdd}>Crear</button>}
+    </form>
+}
+
+export default Form;
